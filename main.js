f2268e2d5475b8ba91c7a5c1e188e7e949ace46a
Giovana Gouvea <giovanagouvea366@gmail.com>
2025-05-04 15:49:19 -0300

    3 finalizado, exemplo 4 iniciado

--- main.js
+++ main.js
@@ -10,10 +10,34 @@ function mostrarAlerta(){
 mostrarAlerta()
 */
 
-//Exemplo 2 
+
+/*Exemplo 2 
 
 function somar (valorA, valorB){
     alert (valorA+valorB)
 }
 
 somar(20,12)
+*/
+
+
+/*Exemplo 3 
+
+function mensagemBoasVindas(nome,cargo){
+    return `Seja bem-vindo(a) ${cargo} ${nome}`
+}
+
+let nome = prompt("Digite o nome do funcionario: ")
+let cargo = prompt("Digite o cargo do funcionario: ")
+
+alert (mensagemBoasVindas(nome,cargo))
+console.log (mensagemBoasVindas(nome,cargo))
+
+*/
+
+/* */
+
+/* Exemplo 4
+
+
+*/
