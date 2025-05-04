6194cb0b246f5c84832a21aa623ff68dd6b2e4e5
Giovana Gouvea <giovanagouvea366@gmail.com>
2025-05-04 15:44:10 -0300

    exemplo 2 pronto, exemplo 3 iniciado

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+
+/*
+Exemplo 1
+
 function mostrarAlerta(){
     let nome = "Marcio"
     alert ("Seja bem-vindo(a), " + nome)
 }
 
 mostrarAlerta()
+*/
+
+//Exemplo 2 
+
+function somar (valorA, valorB){
+    alert (valorA+valorB)
+}
+
+somar(20,12)
