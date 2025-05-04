d1cc252b60fc0ff052d6ad16d0edcc7c47d5b1ce
Giovana Gouvea <giovanagouvea366@gmail.com>
2025-05-04 15:54:06 -0300

    exemplo 4 ok

--- main.js
+++ main.js
@@ -39,5 +39,16 @@ console.log (mensagemBoasVindas(nome,cargo))
 
 /* Exemplo 4
 
+    function multiplicacao (numero1, numero2){
+    return numero1*numero2
+}
 
+let multiplicacao1 = multiplicacao(6,7)
+let multiplicacao2 = multiplicacao(9,10)
+let multiplicacao3 = multiplicacao(12,25)
+
+console.log (multiplicacao1)
+console.log (multiplicacao2)
+console.log (multiplicacao3)
 */
+
